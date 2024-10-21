0bab4e33429bc10247221cf5badb0aa6e43580bb
B-Gerb <gerber.br@northeastern.edu>
2024-10-21 14:57:24 -0400

    day 2 have finished both parts, like both solutions, involve splitting and handing each draw, using regex to split. Through each draw it loops through and checks each individual cube draw. Assuming each is formatted as {color number} splits based on that space.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
   public static void main(String[] args) {
     int dayOne1 = dayone1.stringGather("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day1InfoP1.txt");
     int dayOne2 = dayone1.includeNum2("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day1InfoP1.txt");
-    System.out.println(dayOne2);
+    int dayTwo1 = daytwo.sum("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day2Info.txt");
+    int dayTwo2 = daytwo.powerSet("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day2Info.txt");
+
+    System.out.println(dayTwo2);
   }
 }
---
+++ src/advent/daytwo.java
@@ -0,0 +1,105 @@
+package advent;
+import java.io.File;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Arrays;
+public class daytwo {
+  public static int sum(String fileName) {
+      int total =0;
+      int red = 12;
+      int green = 13;
+      int blue = 14;
+      try{
+        File file = new File(fileName);
+        Scanner s = new Scanner(file);
+        while(s.hasNextLine()){
+          String line = s.nextLine();
+          String first = line.split(":")[0];
+          line = line.split(":")[1];
+          int game = Integer.parseInt(first.split(" ")[1]);
+          List<String> values = Arrays.asList(line.split(";"));
+          boolean correct = true;
+          for (int i=0; i<values.size() && correct; ++i) {
+            String value = values.get(i);
+            List<String> numbers = Arrays.asList(value.split(","));
+            for(int j =0; j<numbers.size(); ++j){
+              if(numbers.get(j).indexOf("red") != -1){
+                if(red < Integer.parseInt(numbers.get(j).split(" ")[1])){
+                  correct = false;
+                }
+
+              }
+              if(numbers.get(j).indexOf("blue") != -1){
+                if(blue < Integer.parseInt(numbers.get(j).split(" ")[1])){
+                  correct = false;
+                }
+              }
+              if(numbers.get(j).indexOf("green") != -1){
+                if(green < Integer.parseInt(numbers.get(j).split(" ")[1])){
+                  correct = false;
+                }
+              }
+            }
+
+          }
+          if(correct){
+            total += game;
+          }
+
+        }
+        return total;
+      }
+      catch(Exception e){
+        System.out.println("error");
+        return 1;
+      }
+  }
+  public static int powerSet(String fileName) {
+    int total =0;
+
+    try{
+      File file = new File(fileName);
+      Scanner s = new Scanner(file);
+      while(s.hasNextLine()){
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        String line = s.nextLine();
+        line = line.split(":")[1];
+        List<String> values = Arrays.asList(line.split(";"));
+        for (int i=0; i<values.size(); ++i) {
+          String value = values.get(i);
+          List<String> numbers = Arrays.asList(value.split(","));
+          for(int j =0; j<numbers.size(); ++j){
+            int num = Integer.parseInt(numbers.get(j).split(" ")[1]);
+            if(numbers.get(j).indexOf("red") != -1){
+              if(red < num){
+                red = num;
+              }
+
+            }
+            if(numbers.get(j).indexOf("blue") != -1){
+              if(blue < num){
+                blue = num;
+              }
+            }
+            if(numbers.get(j).indexOf("green") != -1){
+              if(green < num){
+                green = num;
+              }
+            }
+          }
+
+        }
+        total = total + (red*green*blue);
+
+      }
+      return total;
+    }
+    catch(Exception e){
+      System.out.println("error");
+      return 1;
+    }
+  }
+
+}
