f6bdb1686e9d17c8edec0742667be04d447d36a1
B-Gerb <gerber.br@northeastern.edu>
2024-10-20 22:48:31 -0400

    Completed day 1 part 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import advent.*;
 public class Main {
   public static void main(String[] args) {
+    int dayOne1 = dayone1.stringGather("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day1InfoP1.txt");
 
-    System.out.println("Hello world!");
+    System.out.println(dayOne1);
   }
 }
--- src/advent/dayone1.java
+++ src/advent/dayone1.java
@@ -1,9 +1,42 @@
 package advent;
 
+
+import java.io.File;
+import java.util.Scanner;
 public class dayone1 {
-  int first;
-  int last;
+
   public static int stringGather(String fileName){
-    StringBuilder st
+    int total = 0;
+    boolean firstNum = false;
+    int first =0;
+    int last = 0;
+    try {
+      File file=new File(fileName);
+      Scanner sc=new Scanner(file);
+      while(sc.hasNextLine()){
+        String s = sc.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+          int value = s.charAt(i) - '0';
+          if(value >=0 && value <=9){
+            if(!firstNum){
+              first = value;
+              firstNum = true;
+            }
+            last = value;
+          }
+
+        }
+        first = first*10 + last;
+        total += first;
+        first =0;
+        last = 0;
+        firstNum = false;
+      }
+      return total;
+    }
+    catch(Exception e){
+      System.out.println("error");
+      return -1;
+    }
   }
 }
