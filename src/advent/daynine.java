196133f028904cbde1ffe8f608d83c6f83cc1e4f
B-Gerb <gerber.br@northeastern.edu>
2024-10-29 23:36:07 -0400

    going to start day 9

---
+++ src/advent/daynine.java
@@ -0,0 +1,15 @@
+package advent;
+
+import java.util.Calendar;
+
+public class daynine {
+  public static int OASIS(String fileName){
+    try (Scanner sc = new Scanner(new File(fileName))){
+
+    }
+    catch(Exception e){
+      System.out.println(e);
+      return -1;
+    }
+  }
+}
