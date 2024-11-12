ede16b86a71f124d55bb9a443f4c544a2ff8a466
B-Gerb <gerber.br@northeastern.edu>
2024-11-12 15:50:19 -0500

    Day 16 part 1 finished, had a little trouble figuring out directions and removing infinite cycling.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 public class Main {
   public static void main(String[] args) {
+
 //    int dayOne1 = dayone1.stringGather("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day1InfoP1.txt");
 //    int dayOne2 = dayone1.includeNum2("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day1InfoP1.txt");
 //    int dayTwo1 = daytwo.sum("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day2Info.txt");
@@ -18,7 +19,7 @@ public class Main {
 //    Long dayFive2 = dayfive.rangeSoilWalk("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day5Info.txt");
 //    int daySix1 = daysix.timeSpent("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day6Info.txt");
 //    long daySix2 = daysix.oneRace("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day6Info.txt");
-    dayseven seven = new dayseven();
+//    dayseven seven = new dayseven();
 //    int daySeven1 = seven.handRanking("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day7Info.txt", 1);
 //    int daySeven2= seven.handRanking("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day7Info.txt", 2);
 //    long dayEight1 = dayeight.camelFind("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day8Info.txt");
@@ -27,26 +28,28 @@ public class Main {
 //    int daynine2 = daynine.OASIS("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day9Info.txt", false);
 //      int dayten1 = dayten.farthestDist("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day10Info.txt", 1);
 //    int dayten2 = dayten.farthestDist("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day10Info.txt", 2);
-    dayeleven eleven = new dayeleven();
+//    dayeleven eleven = new dayeleven();
 //    long dayeleven1 = eleven.shortPaths("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day11Info.txt", 1);
 //    long dayeleven2 = eleven.shortPaths("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day11Info.txt", 1000000);
-    day12 twelve = new day12();
+//    day12 twelve = new day12();
 //    long daytwelve1 = twelve.possibilities("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day12Info.text");
 //    long daytwelve2 = twelve.possibilitiespart2("C:\\Users\\high5\\coding\\advent2023\\src\\advent\\day12Info.text");
-    day13 thirteen = new day13();
+//    day13 thirteen = new day13();
 //    int daythirteen1 = thirteen.reflections("\\src\\advent\\day13Info.txt");
 //    int daythirteen2 = thirteen.reflections2("\\src\\advent\\day13Info.txt");
-    day14 fourteen = new day14();
+//    day14 fourteen = new day14();
 //    int dayfourteen1 = fourteen.distance("\\src\\advent\\day14Info.txt");
 //    int dayfourteen2 = fourteen.distanceMultiple("\\src\\advent\\day14Info.txt", 1000000000);
-    day15 fifteen = new day15();
-    Instant start = Instant.now();
+//    day15 fifteen = new day15();
+//    int dayfifteen1 = fifteen.allValues("\\src\\advent\\day15Info.txt");
+//    Instant start = Instant.now();
+//    int dayfifteen2 = fifteen.mappingValues("\\src\\advent\\day15Info.txt");
 
-    int dayfifteen1 = fifteen.allValues("\\src\\advent\\day15Info.txt");
-    int dayfifteen2 = fifteen.mappingValues("\\src\\advent\\day15Info.txt");
+    day16 sixteen = new day16();
+    Instant start = Instant.now();
+    int daysixteen1 = sixteen.reflectionTravel("\\src\\advent\\day16Info");
     Instant end = Instant.now();
     Duration timeSpent = Duration.between(start,end);
-
-    System.out.println("Answer: " + dayfifteen2 + " Time: " + timeSpent);
+    System.out.println("Answer: " + daysixteen1 + " Time: " + timeSpent);
   }
 }
---
+++ src/advent/day16.java
@@ -0,0 +1,133 @@
+package advent;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class day16 {
+  public int  reflectionTravel(String fileName){
+    try(Scanner sc = new Scanner(new File( System.getProperty("user.dir")+ fileName))) {
+      List<List<Character>> grid = new ArrayList<>();
+      List<List<visitor>> visitingPath = new ArrayList<>();
+      while (sc.hasNextLine()) {
+        String s = sc.nextLine();
+        List<Character> toAddGrid = new ArrayList<>();
+        List<visitor> toAddPath = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+          toAddGrid.add(s.charAt(i));
+          toAddPath.add(new visitor(false, ""));
+        }
+        grid.add(toAddGrid);
+        visitingPath.add(toAddPath);
+      }
+      travel(grid, visitingPath, 0, 0, "R");
+      int total = 0;
+      for(int i=0; i<visitingPath.size(); ++i){
+        for (visitor booleans : visitingPath.get(i)) {
+          total += booleans.isVisited() ? 1 : 0;
+        }
+      }
+
+
+      return total;
+    }
+
+    catch(Exception e){
+      System.out.println(e);
+      return -1;
+    }
+  }
+  Map<String, String> backSlash = Map.of( // blackSlash = /
+          "R", "U",
+          "L", "D",
+          "U", "R",
+          "D", "L"
+  );
+  Map<String, String> backSlashReceive = Map.of( // blackSlash = /
+          "R", "RD",
+          "L", "LU",
+          "U", "UL",
+          "D", "RD"
+  );
+  Map<String, String> forwardSlashReceive = Map.of( // fowardslash = \
+          "R", "RU",
+          "L", "LD",
+          "U", "UR",
+          "D", "LD"
+  );
+  Map<String, String> fowardSlash = Map.of( // fowardSlash = \
+          "R", "D",
+          "L", "U",
+          "U", "L",
+          "D", "R"
+  );
+  public void travel(List<List<Character>> grid, List<List<visitor>> visitingPath, int row, int col, String dirc) {
+    boolean leave = false;
+    while (!leave && row >= 0 && row < grid.size() && col >= 0 && col < grid.get(0).size()) {
+      if (grid.get(row).get(col) != '.' && (visitingPath.get(row).get(col).isVisited()
+              && visitingPath.get(row).get(col).getDirc().contains(dirc))) leave = true;
+      else {
+        boolean action = false;
+        if (grid.get(row).get(col) == '-' && (dirc.equals("U") || dirc.equals("D"))) {
+          action = true;
+          visitingPath.get(row).set(col, new visitor(true, dirc+visitingPath.get(row).get(col).getDirc()));
+          travel(grid, visitingPath, row , col-1, "L");
+          travel(grid, visitingPath, row, col+1, "R");
+          leave = true;
+        }
+        if (grid.get(row).get(col) == '|' && (dirc.equals("R") || dirc.equals("L"))) {
+          action = true;
+          visitingPath.get(row).set(col, new visitor(true, dirc+visitingPath.get(row).get(col).getDirc()));
+          travel(grid, visitingPath, row-1, col , "U");
+          travel(grid, visitingPath, row+1, col, "D");
+          leave = true;
+        }
+        if(grid.get(row).get(col) == '/'){
+          visitingPath.get(row).set(col, new visitor(true, backSlashReceive.get(dirc)));
+
+          dirc = backSlash.get(dirc);
+        }
+        if(grid.get(row).get(col) == '\\'){
+          visitingPath.get(row).set(col, new visitor(true, forwardSlashReceive.get(dirc)));
+
+          dirc = fowardSlash.get(dirc);
+        }
+        if (!action) {
+          visitingPath.get(row).set(col, new visitor(true, visitingPath.get(row).get(col).getDirc()));
+          if (dirc.equals("L")) --col;
+          if (dirc.equals("R")) ++col;
+          if (dirc.equals("U")) --row;
+          if (dirc.equals("D")) ++row;
+        }
+      }
+    }
+  }
+  public class visitor{
+    private boolean visited;
+    private String dirc;
+
+    public visitor(boolean visited, String dirc) {
+      this.visited = visited;
+      this.dirc = dirc;
+    }
+
+    public boolean isVisited() {
+      return visited;
+    }
+
+    public void setVisited(boolean visited) {
+      this.visited = visited;
+    }
+
+    public String getDirc() {
+      return dirc;
+    }
+
+    public void setDirc(String dirc) {
+      this.dirc = dirc;
+    }
+  }
+}
